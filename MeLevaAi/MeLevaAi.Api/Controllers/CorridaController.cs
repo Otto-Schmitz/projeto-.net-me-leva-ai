fb5f61487f12ed9318f6c58b03233d6672f958d9
fernando.filho <fernando.filho@cwi.com.br>
2023-05-07 00:12:06 -0300

    Merge branch 'master' of https://git.cwi.com.br/otto.schmitz/crescer-dotnet-trabalho-1

---
+++ MeLevaAi/MeLevaAi.Api/Controllers/CorridaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeLevaAi.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/corridas")]
+    public class CorridaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
