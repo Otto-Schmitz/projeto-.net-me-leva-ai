82adca13e12e0bab603c507605a9b40ddaef0cb8
Otto Schmitz <otto.schmitz@cwi.com.br>
2023-05-07 12:24:21 -0300

    Adicionando CorridaRepository e CorridaService

---
+++ MeLevaAi/MeLevaAi.Api/Repositories/CorridaRepository.cs
@@ -0,0 +1,37 @@
+﻿using MeLevaAi.Api.Domains;
+using System.Xml.Linq;
+
+namespace MeLevaAi.Api.Repositories
+{
+    public class CorridaRepository
+    {
+        private static readonly List<Corrida> _corridas= new();
+
+        public IEnumerable<Corrida> Listar()
+            => _corridas;
+
+        public Corrida? Obter(Guid id)
+            => _corridas.FirstOrDefault(v => v.CorridaID == id);
+
+        public void Adicionar(Corrida corrida)
+        {
+            _corridas.Add(corrida);
+        }
+
+        public void Alterar(Corrida corridaNova)
+        {
+            Remover(corridaNova.CorridaID);
+            Adicionar(corridaNova);
+        }
+
+        public bool Remover(Guid id)
+        {
+            var corrida = Obter(id);
+
+            if (corrida == null)
+                return false;
+
+            return _corridas.Remove(corrida);
+        }
+    }
+}
---
+++ MeLevaAi/MeLevaAi.Api/Services/CorridaService.cs
@@ -0,0 +1,12 @@
+﻿using MeLevaAi.Api.Repositories;
+
+namespace MeLevaAi.Api.Services
+{
+    public class CorridaService
+    {
+        private readonly CorridaRepository _corridaRepository;
+        public CorridaService() {
+            _corridaRepository = new CorridaRepository();
+        }
+    }
+}
